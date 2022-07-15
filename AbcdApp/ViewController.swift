f7f5eb33975b1f5084e585eba81be25f7449bd3c
ajay <ajay01493@gmail.com>
2022-07-15 12:05:15 +0530

    add Some data

---
+++ AbcdApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AbcdApp
+//
+//  Created by Apple on 15/07/22.
+//
+
+import UIKit
+import AbcdTestDemo
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AbcdTestDemo/AbcdTestDemo.h
@@ -0,0 +1,18 @@
+//
+//  AbcdTestDemo.h
+//  AbcdTestDemo
+//
+//  Created by Apple on 15/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AbcdTestDemo.
+FOUNDATION_EXPORT double AbcdTestDemoVersionNumber;
+
+//! Project version string for AbcdTestDemo.
+FOUNDATION_EXPORT const unsigned char AbcdTestDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AbcdTestDemo/PublicHeader.h>
+
+
